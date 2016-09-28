e89a90a92a33f879bdb7907eedc90e779e2898a6
James Shih <gtx1024@gmail.com>
2016-09-28 16:08:37 +0800

    location-map component, maps service and google-maps util

---
+++ tests/unit/utils/google-maps-test.js
@@ -0,0 +1,10 @@
+import MapUtil from 'super-rentals/utils/google-maps';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | google maps');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = MapUtil.create();
+  assert.ok(result);
+});
